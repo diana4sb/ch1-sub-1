720a5314532e18189b8ee71ebadaa1cda88e4230
Diana Samir Bayoumi <diana4sb@bu.edu>
2015-09-11 03:23:33 -0400

    Create in.js

---
+++ in.js
@@ -0,0 +1,8 @@
+   var socket = io.connect('http://localhost'); 
+   socket.on('temps', function (data) { 
+     console.log(data.temp); 
+     socket.emit('my other event', { my: 'data' }); 
+ 
+ 
+     document.getElementById('display_temperature').innerHTML = data.temp; 
+   }); 
